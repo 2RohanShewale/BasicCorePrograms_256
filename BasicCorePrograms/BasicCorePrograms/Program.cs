37e63ba8261f6735af4d171e55128b56c89c5d4d
Rohan Shewale <shewalerohan50@gmail.com>
2023-02-07 10:03:34 +0530

    [Rohan]UC6: Quotient And Remainder

--- BasicCorePrograms/BasicCorePrograms/Program.cs
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -12,7 +12,7 @@ namespace BasicCorePrograms
         public static void Main()
         {
             Console.WriteLine("\n*__________Basic Core Peograms__________*");
-            Console.WriteLine("1. Flipcoin\n2. Leap Year\n3. Power Of Two\n4. Harmonic Number\n5. Prime Factors");
+            Console.WriteLine("1. Flipcoin\n2. Leap Year\n3. Power Of Two\n4. Harmonic Number\n5. Prime Factors\n6. Quotient And Remainder");
             Console.Write("Enter a Options: ");
             int option = Convert.ToInt32(Console.ReadLine());
 
@@ -37,6 +37,10 @@ namespace BasicCorePrograms
                 case 5:
                     PrimeFactors prime = new PrimeFactors();
                     prime.Factors();
+                    break;
+                case 6:
+                    QuotientRemainder quotient = new QuotientRemainder();
+                    quotient.Quotient();
                     break;
 
             }
---
+++ BasicCorePrograms/BasicCorePrograms/QuotientRaminder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class QuotientRemainder
+    {
+        int quotient = 0;
+        int remainder = 0;
+        public void Quotient()
+        {
+            Console.WriteLine("\n*_____Quotient And Remainder_____*");
+            Console.Write("Enter A Divident: ");
+            int divident = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter A Divisor: ");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            quotient = divident / divisor;
+            remainder = divident % divisor;
+
+            Console.WriteLine("Quotient: " + quotient);
+            Console.WriteLine("Remainder: " + remainder);
+
+            Program.Main();
+        }
+    }
+}
